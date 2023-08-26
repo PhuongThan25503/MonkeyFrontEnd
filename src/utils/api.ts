f40e9cd0747fe3743d9ab956f7ab2869cb7048d7
Than Quang Phuong <142188056+PhuongThan25503@users.noreply.github.com>
2023-08-26 23:13:12 +0700

    !add getApiToken component

---
+++ src/utils/api.ts
@@ -0,0 +1,16 @@
+import * as KeyChain from 'react-native-keychain';
+import { SECURE_KEY } from '../config';
+
+export const GetAPIToken = async () => {
+    let apitoken = '';
+    //get token
+    await KeyChain.getGenericPassword({
+      service: SECURE_KEY,
+    }).then(data => {
+      if (data) {
+        apitoken = data.password;
+      } else apitoken = 'no token';
+    });
+    //return the token
+    return apitoken;
+}
