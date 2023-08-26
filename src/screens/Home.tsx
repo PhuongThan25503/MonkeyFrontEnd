0d287e71704f0efb2ab63d4f7fda4ad72bff5072
Than Quang Phuong <142188056+PhuongThan25503@users.noreply.github.com>
2023-08-26 11:38:47 +0700

    !test display api token on home page when successfully logged in

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,21 +1,40 @@
 import * as React from "react";
+import { useState } from "react";
 
 import { View, Text, SafeAreaView, Button } from "react-native";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NativeStackScreenProps } from "@react-navigation/native-stack/lib/typescript/src/types";
+import * as KeyChain from 'react-native-keychain';
 
-type RootStackParamList = {
-  Home: undefined;
-  Login: undefined;
-};
+import { RootStackParamList } from "../navigator/RootStackParamList";
+import { SECURE_KEY } from "../config";
 
-type Props = NativeStackScreenProps<RootStackParamList, 'Home'> // define the types
+type HomeScreenNavigationProp = NativeStackScreenProps<RootStackParamList, 'Home'> //generic type in typescrypt
+
+type Props = {
+  navigation: HomeScreenNavigationProp;
+}
 
 function Home({ navigation }: Props) {
+  const [apiToken, setApiToken] = useState('Okeee');
+  try{
+    //get token
+    KeyChain.getGenericPassword({
+      service: SECURE_KEY,
+    }).then(data => {
+      if(data){
+        setApiToken(data.password);
+      }
+    });
+  } catch (error){
+    console.log(error);
+  }
+
   return (
     <SafeAreaView>
       <View>
+        <Text style={{color:'black'}}>
+          API token : {apiToken}
+        </Text>
         <Text>
           Home
         </Text>
