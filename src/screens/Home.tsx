a8ff0ff4127b45a6b9153b00be75f01310b08039
Than Quang Phuong <142188056+PhuongThan25503@users.noreply.github.com>
2023-08-28 17:02:38 +0700

    !update home UI -Button to go to login -Background

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,24 +1,33 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
 
-import { View, Text, SafeAreaView, Button } from "react-native";
+import { View, Text, SafeAreaView, Button, Image } from "react-native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
-import { RootStackParamList } from "../types"; 
+import { RootStackParamList } from "../types";
 import { GetAPIToken } from "../utils/api";
-
-type HomeScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'Home'> //generic type in typescrypt
+import { HomeStyle } from "./styles/HomeStyle";
+import LoginButton from "../components/LoginButton";
 
 type Props = {
-  navigation: HomeScreenNavigationProp;
+  navigation: NativeStackNavigationProp<RootStackParamList>;
 }
 
-function Home({ navigation }: Props) {
+function Home({ navigation }:Props) {
   return (
-    <SafeAreaView>
-      <View>
-        <Button title="Login" onPress={() => navigation.navigate('Login')}></Button>
-        <Button title="User" onPress={() => navigation.navigate('UserPersonalInfo')}></Button>
+    <SafeAreaView style={HomeStyle.screen}>
+      <View style={{}}>
+        <Image style={HomeStyle.backGround} resizeMode="contain" source={require('../assets/monkey-junior.png')}></Image>
+        <View style={HomeStyle.header}>
+          <View style={HomeStyle.headerLeft}>
+            <Text>Left part</Text>
+          </View>
+          <View style={HomeStyle.authenticate}>
+            <LoginButton onPress={() => navigation.navigate('Login')} style={HomeStyle.loginButton}></LoginButton>
+            {/* <Button title="Login" onPress={() => navigation.navigate('Login')}></Button>
+          <Button title="User" onPress={() => navigation.navigate('UserPersonalInfo')}></Button> */}
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
