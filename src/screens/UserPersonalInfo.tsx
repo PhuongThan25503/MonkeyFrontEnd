f0e7ed136c54f5056d9cd0aace463c215af8df97
Than Quang Phuong <142188056+PhuongThan25503@users.noreply.github.com>
2023-08-28 14:33:07 +0700

    Personal info fetchiing data

--- src/screens/UserPersonalInfo.tsx
+++ src/screens/UserPersonalInfo.tsx
@@ -6,7 +6,9 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { IP } from '../config';
 import axios from 'axios';
 import { GetAPIToken } from '../utils/api';
-import { View , Text} from 'react-native';
+import { View , Text, StyleSheet} from 'react-native';
+import { UserStyle } from './styles/UserPersonalInfoStyle';
+import { User } from '../types';
 
 type UserScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'UserPersonalInfo'>
 
@@ -15,13 +17,19 @@ type Props = {
 }
 
 function UserPersonalInfo({ navigation }: Props) {
-  const [user, setUser] = useState('');
-  const [api_token, setApi_token] = useState('');
+  //define and initialize user
+  const [user, setUser] = useState<User>({
+    id: 0,
+    fullname: 'N/A',
+    phone: 'N/A',
+    email: 'N/A',
+    address: 'N/A',
+  });
 
   //get user info
   async function setUserInfo() {
     try {
-      GetAPIToken().then(token => setApi_token(token));
+      let api_token = await GetAPIToken();
       let apiUrl = IP + '/api/getPersonalInfo';
       let config = {
         headers: { Authorization: `Bearer ${api_token}` }
@@ -39,11 +47,30 @@ function UserPersonalInfo({ navigation }: Props) {
   }, [])
 
   return (
-    <SafeAreaView>
-      <View>
-        <Text style={{color: 'black'}}>
-          {JSON.stringify(user)}
-        </Text>
+    <SafeAreaView style={UserStyle.screen}>
+      <View style={UserStyle.subOuterBound}>
+        <View style={UserStyle.avatarBound}>
+          <View style={UserStyle.fakeAvatar}>
+            <Text style={UserStyle.textAvatar}>{user.fullname.charAt(0)}</Text>
+          </View>
+        </View>
+        <View style={UserStyle.infoBound}>
+          <View style={StyleSheet.compose(UserStyle.infoField, UserStyle.nameField)}>
+            <Text style={UserStyle.textName}>{user.fullname}</Text>
+          </View>
+          <View style={UserStyle.infoField}>
+            <Text style={UserStyle.fieldTitle}>Phone: </Text>
+            <Text style={UserStyle.text}>{user.phone}</Text>
+          </View>
+          <View style={UserStyle.infoField}>
+            <Text style={UserStyle.fieldTitle}>Email: </Text>
+            <Text style={UserStyle.text}>{user.email}</Text>
+          </View>
+          <View style={UserStyle.infoField}>
+            <Text style={UserStyle.fieldTitle}>Address: </Text>
+            <Text style={UserStyle.text}>{user.address}</Text>
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
