15c06fe0b4d83d097260db609dec24570e9c8cec
Than Quang Phuong <142188056+PhuongThan25503@users.noreply.github.com>
2023-08-28 17:04:04 +0700

    Home style update

---
+++ src/screens/styles/HomeStyle.ts
@@ -0,0 +1,38 @@
+import { Dimensions, StyleSheet } from "react-native";
+
+const {width, height} = Dimensions.get('window');
+
+export const HomeStyle = StyleSheet.create({
+  screen:{
+    backgroundColor: 'white',
+    height: height,
+    flex:1,
+  },
+  header: {
+    flexDirection: 'row',
+  },
+  authenticate: {
+    width: width * 0.1,
+    right:0,
+  },
+  backGround: {
+    width: width,
+    position:'absolute',
+  },
+  loginButton: {
+    right:0,
+    height: width * 0.12,
+    width: width * 0.12,
+    alignItems:'center',
+    justifyContent: 'center',
+    borderRadius: 100,
+    borderWidth: 2,
+    borderColor: 'white',
+    margin: width * 0.015,
+    position: 'absolute',
+    backgroundColor: 'brown'
+  },
+  headerLeft: {
+    width: width *0.9,
+  }
+});
