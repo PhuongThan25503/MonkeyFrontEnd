a6016d95b0bfa1b914c9e4305950942b12aba215
Than Quang Phuong <142188056+PhuongThan25503@users.noreply.github.com>
2023-08-30 10:38:28 +0700

    move login logic to utils/user

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -1,10 +1,21 @@
 import { useState, useEffect } from 'react';
 
 import { getAPIToken } from '../utils/api';
-import { IP } from '../config';
+import { IP, SECURE_KEY } from '../config';
 import axios from 'axios';
 import { User } from '../types';
+import * as Keychain from 'react-native-keychain';
 
+//defaut information for user
+export const defaultUser: User = {
+  id: 0,
+  fullname: 'N/A',
+  phone: 'N/A',
+  email: 'N/A',
+  address: 'N/A',
+}
+
+//promise object for getting an user
 export const getUserInfo = async (): Promise<User> => {
   try {
     let api_token = await getAPIToken();
@@ -22,6 +33,7 @@ export const getUserInfo = async (): Promise<User> => {
   return defaultUser
 }
 
+//custom hook for getting an user
 export const useGetUserInfo = (): User => {
   const [userInfo, setUserInfo] = useState(defaultUser);
 
@@ -45,10 +57,28 @@ export const useGetUserInfo = (): User => {
   return userInfo;
 };
 
-export const defaultUser: User = {
-  id: 0,
-  fullname: 'N/A',
-  phone: 'N/A',
-  email: 'N/A',
-  address: 'N/A',
-}
+//login handler
+export const handleLogin = async (username: String, password: String) => {
+  const API_URL = IP + '/api/authenticate';
+  try {
+    let response = await axios.post(API_URL, {
+      username,
+      password
+    });
+    //handle the response from API
+    let apiToken = await response.data;
+    let { token } = await apiToken;
+
+    //for debugging
+    console.log(apiToken);
+
+    //save the api token 
+    await Keychain.setGenericPassword('apiToken', token, {
+      service: SECURE_KEY,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+//logout handler
