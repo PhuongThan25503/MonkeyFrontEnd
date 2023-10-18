70eadc9a462b9e27f1dc05224421c6078a32db6d
Than Quang Phuong <142188056+PhuongThan25503@users.noreply.github.com>
2023-10-18 17:02:35 +0700

    create menu button

--- src/components/StoryHeader/MenuButton.tsx
+++ src/components/StoryHeader/MenuButton.tsx
@@ -1,21 +1,37 @@
-import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { Animated, Dimensions, StyleSheet, TouchableOpacity, View } from "react-native";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-import React from "react";
+import React, { useRef } from "react";
 import { StoryHeaderStyle } from "./style";
+import TaskContainer from "./TaskContainer";
+import { anim } from "../../utils/animation";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 export default function MenuButton({ navigation, color }: any) {
+  const {width, height} = Dimensions.get('window')
+  const transX = useRef(new Animated.Value(width * 0.5)).current;
+  let toggle = true;
   const handlePress = () => {
-    navigation.navigate('SavedStory')
+    // navigation.navigate('SavedStory')
+    if(toggle){
+      anim(transX, 0, 200);
+      toggle = false;
+    }
+    else{
+      anim(transX, width * 0.5, 200);
+      toggle=true;
+    }
   }
 
   return (
-    <React.Fragment>
+    <View style={StoryHeaderStyle.buttonBound}>
       <TouchableOpacity onPress={() => handlePress()} style={StyleSheet.compose(StoryHeaderStyle.menuButton, { backgroundColor: color })}>
         <MaterialCommunityIcons name="menu" size={25} color={'white'}></MaterialCommunityIcons>
       </TouchableOpacity>
-      {/* <View style={StoryStyle.taskContainer}>
-        <TaskContainer></TaskContainer>
-      </View> */}
-    </React.Fragment>
+      <Animated.View style={StyleSheet.compose(StoryHeaderStyle.taskContainer, {transform:[{translateX : transX}]})}>
+        <TaskContainer onPress={()=>navigation.navigate('SavedStory')} text={'Saved stories'} icon={<MaterialIcons name="cloud-download" size={25}></MaterialIcons>}></TaskContainer>
+        <TaskContainer onPress={()=>navigation.navigate('Setting')} text={'Settings'} icon={<Ionicons name="settings-sharp" size={25}></Ionicons>}></TaskContainer>
+      </Animated.View>
+    </View>
   )
 }
